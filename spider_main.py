0ba8f9a127f1a7fdc986638ac980b93fa3a2da45
Rudy1224 <luhuidi@vip.qq.com>
2015-02-17 20:42:52 +0800

    updated source code
    1. much more user-friendly flow control;
    2. improved term list output;
    3. fixed repeated-summation issue after exectuting 'show' function
    multiple times;
    4. allowed for changing user accounts;
    5. minor grammar bug fixes.

--- spider_main.py
+++ spider_main.py
@@ -2,10 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 
 class NJU_GPA_Spider:
+
     def __init__(self):
+        "choose server and define global variables"
         self.servers = ["http://jwas2.nju.edu.cn:8080/jiaowu", "http://jwas3.nju.edu.cn:8080/jiaowu/",
                    "http://desktop.nju.edu.cn:8080/jiaowu/"]
-        self.server_index = input("choose server [1/ 2/ 3]: ") 
+        self.server_index = int(raw_input("choose server [1/ 2/ 3]: ") )
         self.cookies = {}
         self.termlist = {}
         self.creditstat = []
@@ -14,13 +16,15 @@ class NJU_GPA_Spider:
         self.gpa = 5
         
     def login(self):
-        #stuid = raw_input ("Type in Your Student ID: ")
-        #pwd = raw_input ("Type in the Password: ")
-        auth = {'userName': 121090105, 'password': 121858}
+        "get cookie"
+        stuid = raw_input ("Type in your NJU student ID: ")
+        pwd = raw_input ("Type in the password: ")
+        auth = {'userName': stuid, 'password': pwd}
         r = requests.get(self.servers[self.server_index-1]+"login.do", params = auth)
         self.cookies['JSESSIONID'] = r.headers['set-cookie'][11: 43]
         
     def showTermList(self):
+        "list all courses and calculate gpa"
         termCodes = ['20121', '20122', '20131', '20132', '20141', '20142', '20151', '20152']
         temp = []
         for termcode in termCodes:
@@ -34,11 +38,13 @@ class NJU_GPA_Spider:
             try:
                 self.termlist[temp[9*i+2]] = [int(round(float(temp[9*i+5]))), int(round(float(temp[9*i+6])))]
             except:
-                print unicode(temp[9*i+3]), 'does not have a score!'
-        print self.termlist
-        mySpider.gpa_calc()
+                print unicode(temp[9*i+3]), 'does not have a numeric score!'
+        for key, value in self.termlist.items():
+            print unicode(key), value
+        self.gpa_calc()
         
     def showCreditStat(self):
+        "retrive credit earned"
         r = requests.get(self.servers[self.server_index-1]+
                          "student/studentinfo/achievementinfo.do?method=searchCreditStat",
                          cookies=self.cookies)
@@ -51,8 +57,9 @@ class NJU_GPA_Spider:
         for i in range(4):
             print '\t', unicode(self.creditstat[i]), '\t', self.creditstat[i+5]
         print '\t', unicode(self.creditstat[4]), self.creditstat[9], '\n'
+        self.totalcredits = 0
         for i in range(5, 10):
-             self.totalcredits += self.creditstat[i]
+            self.totalcredits += self.creditstat[i]
         if self.creditstat[9] == 14:
             print "    GE course requirement met."
         else:
@@ -60,23 +67,46 @@ class NJU_GPA_Spider:
         print "    Still need %i credits for graduation." % (150-self.totalcredits)
 
     def gpa_calc(self):
+        "calculate gpa"
+        self.totalcredits = 0
+        self.totalpoints = 0
         for value in self.termlist.values():
             self.totalpoints += value[0]*value[1]
             self.totalcredits += value[0]
         self.gpa = (self.totalpoints/self.totalcredits)/20.0
-        print '\n\n\nObtained %i courses and their scores' % len(self.termlist.keys())
-        print 'Your Current GPA = ', self.gpa
+        print '\nObtained %i courses and their scores.' % len(self.termlist.keys())
+        print 'Your current GPA is %f.' % self.gpa
 
 mySpider = NJU_GPA_Spider()
 mySpider.login()
 while True:
-    flag = raw_input('''Choose function:
+    flag = raw_input('''\nChoose function:
                      \n\t a. Show Term List\t b. Show Credit Status\n>''')
     if flag == 'a':
         mySpider.showTermList()
+        while True:
+            newflag = raw_input("Continue to your credit status? Y/N ")
+            if newflag == 'Y' or newflag == 'y':
+                print '\n'
+                mySpider.showCreditStat()
+                break
+            elif newflag == 'N' or newflag == 'n':
+                break
+            else:
+                print "Please enter 'Y' or 'N' !"
         break
     elif flag == 'b':
         mySpider.showCreditStat()
+        while True:
+            newflag = raw_input("Continue to your credit status? Y/N ")
+            if newflag == 'Y' or newflag == 'y':
+                print '\n'
+                mySpider.showTermList()
+                break
+            elif newflag == 'N' or newflag == 'n':
+                break
+            else:
+                print "Please enter 'Y' or 'N' !"
         break
     else:
-        print 'Please enter \'a\' or \'b\''
+        print 'Please enter \'a\' or \'b\'!\n'
